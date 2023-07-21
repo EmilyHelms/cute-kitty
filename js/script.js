eaedc3313fd272d3ae27512690a5930b97bd6909
Emily Helms <137968201+EmilyHelms@users.noreply.github.com>
2023-07-21 15:23:20 -0400

    Updated JS code, button toggles cat photo on and off

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
+//console.log(button);
+//console.log(cat);
+
+// When user clicks on the button, function is triggered to show cat image
 button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    //console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    //console.log("No");
     cat.classList.add("show");
+    button.innerText = "Bye bye, kitty!";
+    button.classList.remove("disappear");
+  }
 });
